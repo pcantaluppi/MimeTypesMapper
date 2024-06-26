994f0c51d003f3367b3035e48adcca26662f132e
Pascal Cantaluppi <57313326+pcantaluppi@users.noreply.github.com>
2024-06-26 06:04:16 +0200

    Version 1.0.5

--- Infrastructure/Logger.cs
+++
@@ -1,9 +0,0 @@
-namespace MimeTypesMapper.Infrastructure;
-
-public static class Logger
-{
-    public static void Log(string message)
-    {
-        Console.WriteLine($"{DateTime.Now}: {message}");
-    }
-}
